8e3f068856a3ad484e8452733e4f23fb6d0ab393
mleandrot1995 <mleandrot1995@gmail.com>
2026-01-12 21:17:32 +0000

    cambios

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,54 +1,35 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
-
-  # Use https://search.nixos.org/packages to find packages
+  channel = "stable-24.05";
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.nodejs_20
+    pkgs.postgresql
+    pkgs.psmisc # Provee fuser
   ];
-
-  # Sets environment variables in the workspace
   env = {};
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "dsznajder.es7-react-js-snippets"
+      "bradlc.vscode-tailwindcss"
+      "dbaeumer.vscode-eslint"
     ];
-
-    # Enable previews
+    workspace = {
+      onCreate = {
+        setup = ''
+          npm install --prefix backend
+          npm install --prefix frontend2
+        '';
+      };
+    };
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+        web = {
+          command = ["bash" "start.sh"];
+          manager = "web";
+          env = {
+            PORT = "5173";
+          };
+        };
       };
     };
   };
